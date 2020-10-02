3cf7e44599cfa8ce12c3e87d64fc1adda081de85
Jacques von Benecke <jacquesvb71@gmail.com>
2020-10-02 14:01:30 +0800

    Second commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import Banner from "../layout/Banner"
 import Infographic from "../layout/Infographic"
-import FlightSpecials from "../layout/FlightSpecials"
-import HotelSpecials from "../layout/HotelSpecials"
+// import FlightSpecials from "../layout/FlightSpecials"
+// import HotelSpecials from "../layout/HotelSpecials"
 
 const Home = () => {
   
@@ -10,8 +10,8 @@ const Home = () => {
     <Fragment> 
     <div className="home">
     <Banner imageurl={"url(https://images.unsplash.com/photo-1540541338287-41700207dee6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)"} backgroundPos ={"50% 35%"} secondaryText={"deals"}/>
-    <FlightSpecials/>
-    <HotelSpecials/> 
+    {/* <FlightSpecials/> */}
+    {/* <HotelSpecials/>  */}
     <Infographic/> 
     </div>
 
